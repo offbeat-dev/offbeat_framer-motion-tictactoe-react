cd6a8479faa06a4b7d2a9c3d0004ae6c40380b2a
offbeat-dev <109253600+offbeat-dev@users.noreply.github.com>
2023-08-23 11:40:11 -0500

    2 files modified

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -41,7 +41,7 @@ const Square = ({ value, onSquareClick }: ISquareProps) => {
         pathLength: 1,
         opacity: 1,
         transition: {
-          pathLength: { delay, type: 'spring', duration: 1.5, bounce: 0 },
+          pathLength: { delay, type: 'spring', duration: 0.5, bounce: 0 },
           opacity: { delay, duration: 0.01 },
         },
       };
